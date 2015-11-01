8d0ebf99275269ef0fc1f2d8726e8bedd0f89cec
David Gomez <dg.dave@gmail.com>
2015-11-01 00:35:55 -0500

    add constant values and extends

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+import { spacing } from './functions';
+
+export const columns = 12;
+
+export const gutter = 0.5;
+
+export const col = {
+  ...spacing(`${gutter}rem`, 'padding', 'horizontal'),
+  boxSizing: 'border-box',
+  display: 'flex',
+  flexDirection: 'column',
+  flexGrow: 0,
+  flexShrink: 0
+};
+
+export const colFlex = {
+  ...col,
+  flexGrow: 1,
+  flexBasis: 0,
+  maxWidth: `100%`
+};
