e9f03c1613d6a1a65ef8b9d63241393020564333
David Gomez <dg.dave@gmail.com>
2015-11-01 15:10:53 -0500

    add documentation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+/**
+ * Unistyle Flex Grid
+ *
+ * A Simple grid layout system build as Unistyle module and an standalone
+ * CSS Grid System
+ *
+ * David Gómez <dg.dave@gmail.com>
+ *
+ * Unistyle Flex Grid is based on the work of Matthew Simo - SCSS Flex Grid
+ * scss-flex-grid [http://matthewsimo.github.io/scss-flex-grid/]
+ */
+
 import rowWrapper from './row-wrapper';
 import baseColumn from './base-column';
 import flexColumn from './flex-column';
