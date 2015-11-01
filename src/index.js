9307d9123de018948e36568306aad9012477fd8a
David Gomez <dg.dave@gmail.com>
2015-11-01 00:47:17 -0500

    marge all together with the index file

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import rowWrapper from './row-wrapper';
+import baseColumn from './base-column';
+import flexColumn from './flex-column';
+import responsiveColumns from './responsive-columns';
+import responsiveOffsets from './responsive-offsets';
+
+export default [
+  rowWrapper,
+  baseColumn,
+  flexColumn,
+  responsiveColumns,
+  responsiveOffsets
+];
