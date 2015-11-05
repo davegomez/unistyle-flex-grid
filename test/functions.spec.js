78daf5548f0b90bcfd0f5f485d4c2ab1357b2ea4
David Gomez <dg.dave@gmail.com>
2015-11-05 00:36:23 -0500

    Initial testing efforts

--- test/functions.spec.js
+++ test/functions.spec.js
@@ -1,6 +1,47 @@
-import { describe } from 'mocha/mocha';
+/* eslint no-undef:0 */
+
+import { assert, expect } from 'chai';
 import { getPercentage, spacing } from '../src/functions';
 
-describe('getPercentage function', () => {
+describe('helper functions', () => {
+  describe('getPercentage', () => {
+    it('should return', () => {
+      assert.ok(getPercentage(1, 2));
+    });
 
+    it('should return a string', () => {
+      assert.isString(getPercentage(1, 2));
+    });
+
+    it('should return a string with "%" symbol in it', () => {
+      assert.include(getPercentage(1, 2), '%');
+    });
+
+    it('should return a string with length > 7', () => {
+      assert.isAbove(getPercentage(1, 2).length, 7);
+    });
+
+    it('should return a string with length < 11', () => {
+      assert.isBelow(getPercentage(1, 2).length, 11);
+    });
+
+    it('should return the percentage value for each column position', () => {
+      assert.equal('16.66667%', getPercentage(1, 6));
+      assert.equal('33.33333%', getPercentage(2, 6));
+      assert.equal('50.00000%', getPercentage(3, 6));
+      assert.equal('66.66667%', getPercentage(4, 6));
+      assert.equal('83.33333%', getPercentage(5, 6));
+      assert.equal('100.00000%', getPercentage(6, 6));
+    });
+  });
+
+  describe('spacing', () => {
+    it('should return', () => {
+      assert.ok(spacing(24, 'margin', 'vertical'));
+    });
+
+    it('should return an object', () => {
+      assert.isObject(spacing(24, 'margin', 'vertical'));
+    });
+  });
 });
