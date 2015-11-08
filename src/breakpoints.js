3c62d479879c05f046f2e17f9e97e0effe10bb87
David Gomez <dg.dave@gmail.com>
2015-11-08 18:30:20 -0500

    breakpoint unit testijng and refactor

--- src/breakpoints.js
+++ src/breakpoints.js
@@ -1,10 +1,31 @@
 /**
  * List of mobile first breakpoints with its corresponding media query values
  */
-
-export default {
+const defaults = {
   xs: '',
-  sm: '@media only screen and (min-width: 768px)',
-  md: '@media only screen and (min-width: 992px)',
-  lg: '@media only screen and (min-width: 1200px)'
+  sm: 768,
+  md: 992,
+  lg: 1200
 };
+
+/**
+ * Creates an object of media queries based on the options defaults or a passed
+ * custom object in which case also takes an option to make the grid desktop
+ * first
+ * @param {Object} bps - List of custom breakpoints
+ * @param {string} first - Optional Mobile or Desktop first prefix [min || max]
+ * @returns {Object} Object with the namespaces and media queries for the
+ * responsive approach
+ */
+export default function (bps = defaults, first = 'min') {
+  const mediaObject = {};
+
+  Object.keys(bps)
+    .forEach(key =>
+      mediaObject[key] = bps[key] ?
+        `@media only screen and (${first}-width: ${bps[key]}px)` :
+        ''
+    );
+
+  return mediaObject;
+}
--- test/breakpoints.spec.js
+++ test/breakpoints.spec.js
@@ -1,3 +1,68 @@
-/**
- * Created by dave on 8/11/15.
- */
+/* eslint no-undef:0 */
+
+import { assert, expect } from 'chai';
+import setBreakpoints from '../src/breakpoints';
+
+suite('breakpoints', () => {
+  let defaultBps;
+  let customBps;
+
+  const custom = {
+    'extra-small': '',
+    small: 1024,
+    medium: 1200,
+    large: 1600
+  };
+
+  setup(() => defaultBps = setBreakpoints());
+  setup(() => customBps = setBreakpoints(custom, 'max'));
+
+  test('should be defined', () => {
+    assert.ok(defaultBps);
+    assert.ok(customBps);
+  });
+
+  test('should be an object', () => {
+    assert.isObject(defaultBps);
+    assert.isObject(customBps);
+  });
+
+  test('should have property', () => {
+    assert.property(defaultBps, 'xs');
+    assert.property(defaultBps, 'sm');
+    assert.property(defaultBps, 'md');
+    assert.property(defaultBps, 'lg');
+    assert.property(customBps, 'extra-small');
+    assert.property(customBps, 'small');
+    assert.property(customBps, 'medium');
+    assert.property(customBps, 'large');
+  });
+
+  test('should be a string value', () => {
+    assert.isString(defaultBps.xs);
+    assert.isString(defaultBps.sm);
+    assert.isString(defaultBps.md);
+    assert.isString(defaultBps.lg);
+    assert.isString(customBps['extra-small']);
+    assert.isString(customBps.small);
+    assert.isString(customBps.medium);
+    assert.isString(customBps.large);
+  });
+
+  test('should be an empty string', () => {
+    assert.lengthOf(defaultBps.xs, 0);
+    assert.lengthOf(customBps['extra-small'], 0);
+  });
+
+  test('should have the text in property value', () => {
+    assert.include(defaultBps.sm, '768');
+    assert.include(defaultBps.md, '992');
+    assert.include(defaultBps.lg, '1200');
+    assert.include(customBps.small, '1024');
+    assert.include(customBps.medium, '1200');
+    assert.include(customBps.large, '1600');
+    assert.include(customBps.small, 'max-width');
+    assert.include(customBps.medium, 'max-width');
+    assert.include(customBps.large, 'max-width');
+  });
+});
